84c5a4635398c689aeffe5cd60ac96e471c60e5e
shanzewang <127651938+shanzewang@users.noreply.github.com>
2025-04-22 20:44:07 +0800

    Delete AAE6102_LIZhengdao_24039157R_A2-main directory

--- AAE6102_LIZhengdao_24039157R_A2-main/geo/R_ecef_enu.m
+++
@@ -1,9 +0,0 @@
-function R = R_ecef_enu(ref_llh_coord)
-    %% Written by Zhengdao LI (zhengda0.li@connect.polyu.hk)
-    phi    = ref_llh_coord(1);
-    lambda = ref_llh_coord(2);
-
-    R = [-sin(lambda)          cos(lambda)                  0;
-         -sin(phi)*cos(lambda) -sin(phi)*sin(lambda) cos(phi);
-         cos(phi)*cos(lambda)  cos(phi)*sin(lambda)  sin(phi)];
-end
